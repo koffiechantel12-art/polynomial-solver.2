4686766a7e80e119667678dbed494a86fc477c61
koffiechantel12-art <koffiechantel12@gmail.com>
2026-01-20 17:20:06 +0000

    Update history_supabase.py

--- history_supabase.py
+++ history_supabase.py
@@ -1,11 +1,11 @@
 from supabase import Client
 
 def add_history(supabase: Client, user_email: str, expression: str, roots: str):
-    supabase.table("history").insert({
-        "user_email": user_email,
-        "expression": expression,
-        "roots": roots
-    }).execute()
+   supabase.table("history").insert({
+    "user_email": user_email,
+    "expression": expr,
+    "roots": roots
+}).execute()
 
 
 def get_history(supabase: Client, user_email: str | None = None):
